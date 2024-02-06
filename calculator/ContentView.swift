2fbca4545015303c4c174f4f6c8a943056e3ba74
Kübra Bahadır <156602024+KubraBahadir84@users.noreply.github.com>
2024-02-06 15:49:22 +0300

    Changes have been made to the interface.
    The buttons' colors and sizes have been adjusted. 
    VStack and HStack configurations have been set, fixing the layout. 
    DidTap function has been added. 
    State variables have been created.

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  calculator
 //
-//  Created by Kübra Bahadır on 9.01.2024.
+//  Created by Kübra Bahadır on 4.02.2024.
 //
 
 import SwiftUI
@@ -27,16 +27,36 @@ enum CalcButton:String {
     case decimal = ","
     case percent = "%"
     case negative = "-/+"
-}
+    
+    var buttonColor: Color {
+        switch self {
+      case .add, .subtract, .mutliply, .divide, .equal:
+           return .orange
+       case .clear, .negative, .percent:
+          return Color(.lightGray)
+     default:
+        
+            
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        }
+        
+        enum Operation {
+            case add, subtract, multiply, divide, none
+        }
+    
 
 struct ContentView: View {
     
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
+    
     let buttons: [[CalcButton]] = [
-        [.clear, .negative, .percent],
-        [.seven, .eight, .nine],
-        [.four, .five, .six],
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .mutliply],
+        [.four, .five, .six, .subtract],
         [.one, .two, .three],
-        [.zero, .zero, .decimal]
+        [.zero, .decimal, .equal]
     ]
     
     var body: some View {
@@ -44,34 +64,70 @@ struct ContentView: View {
             Color.black.edgesIgnoringSafeArea(.all)
             
             VStack {
+                Spacer()
+                
                 // Text display
                 HStack {
                     Spacer()
-                    Text ("0")
+                    Text (value)
                         .bold()
-                        .font(.system(size: 64))
+                        .font(.system(size: 100))
                         .foregroundColor(.white)
                 }
                 .padding()
             
                 // Anlamadik Usta
                 ForEach(buttons,id: \.self) { row in
-                    HStack {
+                    HStack(spacing: 12) {
     ForEach(row, id: \.self) { item in
     Button(action: {
+        self.didTap(button: item)
     }, label: {
-        Text(item.rawValue)
+        Text(item.rawValue) //burda bi sikinti var
             .font(.system(size: 32))
-            .frame(width: 70, height: 70)
-            .background(Color.orange)
+            .frame(
+                width: self.buttonWidth(item: item),
+                height: self.buttonHeight()
+            )
+            .background(item.buttonColor)
             .foregroundColor(.white)
-            .cornerRadius(35)
+            .cornerRadius(self.buttonWidth(item: item)/2)
     })
                     }
                 }
+                    padding(.bottom, 3)
             }
         }
     }
+}
+    
+    func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            break
+        case .clear:
+            self.value = "0"
+           break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else{
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+    
+    func buttonWidth(item:CalcButton) -> CGFloat {
+        if item == .zero {
+            return (UIScreen.main.bounds.width - (4*12)) / 4) * 2
+        }
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
+    func buttonHeight() -> CGFloat{
+        return (UIScreen.main.bounds.width - (5*12)) / 4
 }
 }
 /*struct ContenView_Previews: PreviewProvider {
